7c911db6344d466f2873bf1f64195e60e30d8886
Nothing98512 <123211206+Nothing98512@users.noreply.github.com>
2026-09-06 11:58:51 -0400

    augh

---
+++ Content.Shared/Materials/PhysicalCompositionComponent.Trauma.cs
@@ -0,0 +1,10 @@
+﻿using Content.Shared.Chemistry.Reagent;
+using Content.Shared.FixedPoint;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Materials;
+
+public sealed partial class PhysicalCompositionComponent : Component
+{
+
+}
